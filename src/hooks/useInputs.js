0404f973326a8224640a84d48345373bcd5995ad
shinhonggyu <zowoz0@naver.com>
2022-01-29 16:29:22 +0900

    커스텀 Hooks 만들기

---
+++ src/hooks/useInputs.js
@@ -0,0 +1,47 @@
+// useState, useEffect, useReducer, useCallback 등 Hooks 를 사용하여 원하는 기능을 구현해주고,
+// 컴포넌트에서 사용하고 싶은 값들을 반환
+
+import { useReducer, useCallback } from "react";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CHANGE_INPUTS":
+      return {
+        ...state,
+        [action.name]: action.value,
+      };
+    case "RESET":
+      return Object.keys(state).reduce((acc, current) => {
+        acc[current] = "";
+        // 리듀서 함수의 반환 값은 누산기에 할당
+        return acc;
+      }, {});
+    default:
+      return state;
+  }
+}
+
+function useInputs(initialForm) {
+  const [form, dispatch] = useReducer(reducer, initialForm);
+  console.log("form", form);
+
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    dispatch({
+      type: "CHANGE_INPUTS",
+      name,
+      value,
+    });
+  }, []);
+
+  const reset = useCallback(() => {
+    dispatch({
+      type: "RESET",
+      payload: initialForm,
+    });
+  }, [initialForm]);
+
+  return [form, onChange, reset];
+}
+
+export default useInputs;
